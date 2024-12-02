9a5fc5f3eb173bac4bfa2a9460c1902c4ece0d28
masizsa <ihza.nk.160903@gmail.com>
2024-12-02 21:15:30 +0700

    [fix]: fix error when remove folder

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from .yolov5_model import detect_padi
 from .cnn_model import classify_disease
 import os
+import shutil
 
 bp = Blueprint('routes', __name__)
 
@@ -23,12 +24,18 @@ def detect():
         disease_results = []
 
         # Jalankan identifikasi penyakit pada setiap deteksi
-        label, confidence = classify_disease( image_path)
+        label, confidence = classify_disease(image_path)
         disease_results.append({'label': label, 'confidence': confidence})
 
-        # return ({'detections': detections, 'disease_results': disease_results})
-        # return ({'detections': detections})
-        return ({'disease_results': disease_results})
+        # Hapus folder ./static/uploads/ dan ./static/results/
+        for folder in ['./static/uploads', './static/results']:
+            if os.path.exists(folder):
+                shutil.rmtree(folder)
+
+        return jsonify({'disease_results': disease_results})
     else:
+        for folder in ['./static/uploads', './static/results']:
+            if os.path.exists(folder):
+                shutil.rmtree(folder)
+
         return jsonify({'message': 'No rice leaf detected'}), 200
-    
